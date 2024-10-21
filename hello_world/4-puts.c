f005487d1c68ddd9588268bc995ec1e18e4f81db
maxine-janka <maxinejanka@gmail.com>
2024-10-21 03:44:22 +0000

    A C program that uses puts to print a string

---
+++ hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * puts - Prints a string
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle")
+	return (0);
+}
+
