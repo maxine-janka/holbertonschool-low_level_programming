c87ddc0a06630bb6eadeb2412ca7dab4ec84ead4
maxine-janka <maxinejanka@gmail.com>
2024-10-22 03:46:12 +0000

    Program that prints integrers 0-9 suing putchar

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Prints integers `from  0-9 using putchar
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void);
+
+{
+	int n;
+
+	for (n = '0' ; n <= '9' ; n++)
+	{
+	putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
