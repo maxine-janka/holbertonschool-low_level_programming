70f3068703812094586c5845faa446826e392e52
maxine-janka <maxinejanka@gmail.com>
2024-10-22 03:37:15 +0000

    Program that prints numbers from 0-9

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Prints integers from 0-9
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0 ; n <= 9 ; n++)
+	{
+	printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
