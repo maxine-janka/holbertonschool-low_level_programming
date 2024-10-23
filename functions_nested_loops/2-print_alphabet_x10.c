5ad3ba100921960af5b94f3f2114c26faa41ba44
maxine-janka <maxinejanka@gmail.com>
2024-10-23 07:32:40 +0000

    Fix syntax error

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -13,9 +13,9 @@ void print_alphabet_x10(void)
 	char alpha;
 	int line;
 
-	for (line = '0' ; line < 10 ; line++)
+	for (line = 0; line < 10; line++)
 	{
-		for (alpha = 'a' ; alpha <= 'z' ; alpha++)
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
 		{
 			_putchar(alpha);
 		}
