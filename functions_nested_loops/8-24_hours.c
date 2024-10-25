dbbcc28f907e66c1f2c014c2ee0ee5278fd9d361
maxine-janka <maxinejanka@gmail.com>
2024-10-25 02:39:14 +0000

    Test output fix

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -16,6 +16,7 @@ void jack_bauer(void)
 	int hours_end = 24;
 	int hours_first_digit;
 	int hours_second_digit;
+	float NULL;
 
 		while (hours < hours_end)
 		{
@@ -33,6 +34,10 @@ void jack_bauer(void)
 				minutes_first_digit = (minutes - minutes_second_digit) / 10;
 				minutes_second_digit = (minutes % 10);
 
+				if (minutes_second_digit == NULL)
+				{
+				_putchar('0');
+				}
 				_putchar(48 + minutes_first_digit);
 				_putchar(48 + minutes_second_digit);
 				_putchar('\n');
