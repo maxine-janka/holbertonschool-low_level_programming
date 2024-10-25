b7ea712736a01b28e1d316706aed669561ec0d0c
maxine-janka <maxinejanka@gmail.com>
2024-10-25 02:16:21 +0000

    Fix test for compilation errors

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -12,35 +12,34 @@ void jack_bauer(void)
 	int minutes_end = 60;
 	int minutes_first_digit;
 	int minutes_second_digit;
-
 	int hours = 0;
 	int hours_end = 24;
 	int hours_first_digit;
 	int hours_second_digit;
 
-		while (hours < hours_end) //hours or rows
+		while (hours < hours_end)
 		{
-			hours_first_digit = (hours - hours_second_digit) / 10; //calculate hours
+			hours_first_digit = (hours - hours_second_digit) / 10;
 			hours_second_digit = (hours % 10);
-			
-			minutes = 0; //reset the minutes for each hour
 
-			while (minutes < minutes_end) //for every hour or row  I want 59 minutes (columns)
+			minutes = 0;
+
+			while (minutes < minutes_end)
 			{
-				_putchar(48 + hours_first_digit); //print hours first
-                        	_putchar(48 + hours_second_digit);
-                        	_putchar(':');
+				_putchar(48 + hours_first_digit);
+				_putchar(48 + hours_second_digit);
+				_putchar(':');
 
-				minutes_first_digit = (minutes - minutes_second_digit) / 10; //calculate minutes
+				minutes_first_digit = (minutes - minutes_second_digit) / 10;
 				minutes_second_digit = (minutes % 10);
-				
-				_putchar(48 + minutes_first_digit); //print minutes
+
+				_putchar(48 + minutes_first_digit);
 				_putchar(48 + minutes_second_digit);
 				_putchar('\n');
 
-				minutes = minutes + 1; //add a minute each time up to 59 then go back to hours
-			}	
-		
-			hours = hours + 1; //add an hour each time after every 59 minutes
+				minutes = minutes + 1;
+			}
+
+			hours = hours + 1;
 		}
 }
