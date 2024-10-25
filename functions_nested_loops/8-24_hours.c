4e17ca97279e1d0d89ebbff6247dc436c4501736
maxine-janka <maxinejanka@gmail.com>
2024-10-25 03:32:42 +0000

    Output fix using for loop

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -8,38 +8,18 @@
 
 void jack_bauer(void)
 {
-	int minutes = 0;
-	int minutes_end = 60;
-	int minutes_first_digit;
-	int minutes_second_digit;
-	int hours = 0;
-	int hours_end = 24;
-	int hours_first_digit;
-	int hours_second_digit;
-
-		while (hours < hours_end)
+	int hours, minutes;
+		
+	for (hours = 0; hours < 24; hours++)
+	{
+		for (minutes = 0; minutes < 60; minutes++)
 		{
-			hours_first_digit = (hours - hours_second_digit) / 10;
-			hours_second_digit = (hours % 10);
-
-			minutes = 0;
-
-			while (minutes < minutes_end)
-			{
-				_putchar('0' + hours_first_digit);
-				_putchar('0' + hours_second_digit);
+				_putchar((hours / 10) + '0');
+				_putchar((hours % 10) + '0');
 				_putchar(':');
-
-				minutes_first_digit = (minutes - minutes_second_digit) / 10;
-				minutes_second_digit = (minutes % 10);
-
-				_putchar('0' + minutes_first_digit);
-				_putchar('0' + minutes_second_digit);
+				_putchar((minutes / 10) + '0');
+				_putchar((minutes % 10) + '0');
 				_putchar('\n');
-
-				minutes = minutes + 1;
-			}
-
-			hours = hours + 1;
 		}
+	}
 }
