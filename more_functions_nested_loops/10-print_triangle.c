a2b155a1924cbfa631517be5d7082fdc188e8a52
maxine-janka <maxinejanka@gmail.com>
2024-10-28 10:11:25 +0000

    Print right-aligned triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_triangle - Prints a triangle, followed by new line
+ *
+ * @size: Size of triangle
+ *
+ * Return: Nothing
+ */
+
+void print_triangle(int size)
+
+{
+	int row;
+	int space;
+
+	if (size > 0)
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (space = row; space < size; space++)
+			{
+				_putchar(' ');
+			}
+			for (space = 1; space <= row; space++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
+}
