0e98bfc8f799830f481f55a6bb46bfbc40c3a2cb
maxine-janka <maxinejanka@gmail.com>
2024-10-29 04:52:12 +0000

    Add header file and README

---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,12 @@
+#ifndef HOLBERTON_HEADER_FILE
+#define HOLBERTON_HEADER_FILE
+
+/**
+ * Description: Header file containing fucntion prototypes
+ * used in C - Pointers, arrays and strings
+ *
+ */
+
+void reset_to_98(int *n);
+
+#endif
