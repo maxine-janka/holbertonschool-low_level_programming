a79f8e3c38ff76550e6acda3210b646c233c9418
maxine-janka <maxinejanka@gmail.com>
2024-10-30 06:28:48 +0000

    Function that prints the second half of a string

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,65 @@
+#include "main.h"
+
+/**
+ * _strlen - Returns the length of a string
+ *
+ * @s: Input string to determine the length
+ *
+ * Return: Nothing
+ */
+
+int  _strlen(char *s)
+{
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+	return (length);
+}
+
+/**
+ * _puts - Prints a string, followed by a new line
+ *
+ * @str: String to be printed
+ *
+ * Return: Nothing
+ */
+
+void _puts(char *str)
+
+{
+	int ch;
+
+	for (ch = 0; str[ch] != '\0'; ch++)
+	{
+		_putchar(str[ch]);
+	}
+	_putchar('\n');
+}
+
+/**
+ * puts_half - Prints the second half of a string
+ *
+ * @str: Input string to print the second half
+ *
+ * Return: Nothing
+ *
+ */
+
+void puts_half(char *str)
+{
+	int length = _strlen(str);
+	char *half;
+
+	if (length % 2 == 0)
+	{
+		half = &str[length / 2];
+	}
+	else
+	{
+		half = &str[(length - 1 / 2)];
+	}
+	_puts(half);
+}
