75cdd50dee9534e915eed1578608e2f042cd03b6
maxine-janka <maxinejanka@gmail.com>
2024-10-30 00:13:54 +0000

    Function thawqt prints a string in reverse

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * print_rev - Prints a string in reverse, followed by a new line
+ *
+ * @s: String to be printed in reverse
+ *
+ * Return: Nothing
+ */
+
+void print_rev(char *s)
+{
+	int length = 0;
+	int count;
+
+	while (s[count] != '\0')
+	{
+		count++;
+		length++;
+	}
+
+	for (count = length - 1; count >= 0; count--)
+	{
+		_putchar(s[count]);
+	}
+	_putchar('\n');
+}
