cdf4090a5f9bb6e3b6170ea9702a0e24b3590d8e
maxine-janka <maxinejanka@gmail.com>
2024-11-01 02:30:03 +0000

    Function that concatonates two strings

---
+++ 1_pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strcat: Concatenates two strings
+ *
+ * @src: String to appended
+ * @dest: Pointer to concatonated resulting string 
+ *
+ * Return: Pointer to the concatonated string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int d_index = 0;
+	int s_index = 0;
+
+	while (dest[d_index] != '\0')
+	{
+		d_index++;
+	}
+
+	while (src[s_index] != '\0')
+	{
+		dest[d_index] = src[s_index];
+		d_index++;
+		s_index++;
+	}
+	dest[d_index] = '\0';
+	return (dest);
+
+}
