f73674a9c6de66ccc367521fc06d4a4f89535602
maxine-janka <maxinejanka@gmail.com>
2024-11-04 22:36:59 +0000

    Fix test

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -13,17 +13,18 @@
 char *_strchr(char *s, char c)
 {
 
-	while (*s <= '\0')
+	while (*s >= '\0')
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
+	
+		if (c == '\0')
+		{
+			return (s);
+		}
 		s++;
-	}
-	if (c == '\0')
-	{
-		return (s);
 	}
 	return (0);
 }
