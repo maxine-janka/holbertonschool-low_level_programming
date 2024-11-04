0a4b3e283aa7534570a66817a37215f5474adf2f
maxine-janka <maxinejanka@gmail.com>
2024-11-04 22:11:46 +0000

    Fix test - account for null terminator as parameter

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -13,13 +13,17 @@
 char *_strchr(char *s, char c)
 {
 
-	while (*s != '\0')
+	while (*s <= '\0')
 	{
 		if (*s == c)
 		{
 			return (s);
 		}
 		s++;
+	}
+	if (c == '\0')
+	{
+		return (s);
 	}
 	return (0);
 }
