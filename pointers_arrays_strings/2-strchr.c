ba3acf241620c2698dd438bacf494577213a7c8d
root <maxinejanka@gmail.com>
2024-11-04 05:58:08 +0000

    Remove NULL stddef

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stddef.h>
 
 /**
  * _strchr - locates a character in a string
@@ -22,5 +21,5 @@ char *_strchr(char *s, char c)
 		}
 		s++;
 	}
-	return NULL;
+	return (0);
 }
