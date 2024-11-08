dfbc44c7305e771f42dae42526824e07fef11391
maxine-janka <maxinejanka@gmail.com>
2024-11-08 07:18:43 +0000

    Fix declaraction error in second for loop

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 
 	for (i = 1; i < argc; i++)
 	{
-		for (int j = 0; argv[i][j] != '\0'; j++)
+		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (!isdigit(argv[i][j]))
 			{
