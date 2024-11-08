a0fa7a13bc29144cd3de20eac1bd027a528cad92
maxine-janka <maxinejanka@gmail.com>
2024-11-08 07:15:43 +0000

    Program that prints sum of two +ve numbers

---
+++ argc_argv/4-add.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+
+/**
+ * main - Prints the sum of postive numbers
+ *
+ * @argc: Number of arguments supplied (size).
+ * @argv: An array of arguments supplied.
+ *
+ * Return: 0 (Success).
+ *         Return 1 if any arguments are not posotive numbers
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+	int sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (int j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+	return (0);
+}
