10f6d8ea34c35707e4a16ff1a4157aaa6dec864b
maxine-janka <maxinejanka@gmail.com>
2024-11-13 04:39:01 +0000

    Add files

---
+++ more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This projects contains tasks for programming in C - More malloc, free. For Holberton School Australia. By Maxine Janka
---
+++ more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef HOLBERTON_HEADER_FILE
+#define HOLBERTON_HEADER_FILE
+
+void *malloc_checked(unsigned int b);
+char *string_nconcat(char *s1, char *s2, unsigned int n);
+void *_calloc(unsigned int nmemb, unsigned int size);
+void *_calloc(unsigned int nmemb, unsigned int size);
+
+#endif
