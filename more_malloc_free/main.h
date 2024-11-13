66b32175e257f3c3b1ed59587442ef25f6093ae7
maxine-janka <maxinejanka@gmail.com>
2024-11-13 09:40:28 +0000

    Update header file

--- more_malloc_free/main.h
+++ more_malloc_free/main.h
@@ -7,7 +7,4 @@ void *_calloc(unsigned int nmemb, unsigned int size);
 void *_calloc(unsigned int nmemb, unsigned int size);
 
 int _strlen(char *s);
-char *_strncpy(char *dest, char *src, int n);
-char *_strncat(char *dest, char *src, int n);
-
 #endif
