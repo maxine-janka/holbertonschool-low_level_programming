ad5928546116ccc415f8d3108f76679f99e5b7f6
maxine-janka <maxinejanka@gmail.com>
2024-11-17 05:03:52 +0000

    Fix - change order of members

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -21,6 +21,8 @@ void print_dog(struct dog *d)
 			printf("Name: %s\n", d->name);
 		}
 
+		printf("Age: %f\n", d->age);
+
 		if (d->owner == NULL)
 		{
 			printf("Owner: (nil)\n");
@@ -30,6 +32,5 @@ void print_dog(struct dog *d)
 			printf("Owner: %s\n", d->owner);
 		}
 
-		printf("Age: %f\n", d->age);
 	}
 }
