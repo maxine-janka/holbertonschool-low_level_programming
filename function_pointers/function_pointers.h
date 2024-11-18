181971731cbce409760a3348064c7974ddd4e218
maxine-janka <maxinejanka@gmail.com>
2024-11-18 06:45:33 +0000

    Test - betty fix

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -4,11 +4,7 @@
 /**
  * Description: Header file containing function prototypes
  *              used in C - Function pointers.
- *
  */
 
 void print_name(char *name, void (*f)(char *));
-
-int _putchar(char c);
-
 #endif
