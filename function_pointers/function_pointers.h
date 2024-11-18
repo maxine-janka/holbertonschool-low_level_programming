ca0587e5c7e6ec6088b285b25094d8ee3b690627
maxine-janka <maxinejanka@gmail.com>
2024-11-18 04:56:37 +0000

    Add header file

---
+++ function_pointers/function_pointers.h
@@ -0,0 +1,14 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+ * Description: Header file containing function prototypes
+ *              used in C - Function pointers.
+ *
+ */
+
+void print_name(char *name, void (*f)(char *));
+
+int _putchar(char c);
+
+#endif
