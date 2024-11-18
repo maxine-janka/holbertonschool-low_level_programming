2df913a441d120f041d1466ef727cdb300c194ce
maxine-janka <maxinejanka@gmail.com>
2024-11-18 23:54:54 +0000

    Fix - betty style

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -17,15 +17,15 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (array == NULL || cmp == NULL || size <=0)
+	if (array == NULL || cmp == NULL || size < 0)
 	{
 		return (-1);
 	}
-	
+
 	i = 0;
 	while (i < size)
 	{
-		if(cmp(array[i] != 0))
+		if (cmp(array[i]) != 0)
 		{
 			return (i);
 		}
