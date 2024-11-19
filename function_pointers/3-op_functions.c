8a654c359c4b8e56ecb8b5041a5342e7958412ad
maxine-janka <maxinejanka@gmail.com>
2024-11-19 05:04:06 +0000

    Code updates - TBC

---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,30 @@
+#include "calc.h"
+
+/**
+ * op_add - Adds two integers.
+ * @a: First integer.
+ * @b: Second integer.
+ *
+ * Return: The sum of a + b.
+ */
+
+int op_add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * op_sub - Subtracts one integer from another.
+ *
+ * @a: Integer to subtract from.
+ * @b: Integer to substract.
+ *
+ * Return: The difference of a - b.
+ */ 
+
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+
+
