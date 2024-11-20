e6b0604951ff8c3cf4c8899d7a40ea929bc67759
maxine-janka <maxinejanka@gmail.com>
2024-11-20 02:24:19 +0000

    Function definitions for ops

--- function_pointers/3-op_functions.c
+++ function_pointers/3-op_functions.c
@@ -27,4 +27,44 @@ int op_sub(int a, int b)
 	return (a - b);
 }
 
+/* op_mul - Multiplies two integers and returns the product.
+ *
+ * @a: First integer to multiply.
+ * @b: Second integer to multiply.
+ *
+ * Return: The product of a * b.
+ */
 
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+
+
+/**
+ * op_div - Divides two integers and returns the result.
+ *
+ * @a: Integer to divide.
+ * @b: Integeger divisor.
+ *
+ * Return: The result of the division of a by b.
+ */
+
+int op_div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * op_mod - Divide two integers and return the remainder.
+ *
+ * @a: Integer to divide.
+ * @b: Integer divisor.
+ *
+ * Return: The remainder of the division of a by b.
+ */
+
+int op_mod(int a, int b) 
+{
+	return (a % b);
+}
