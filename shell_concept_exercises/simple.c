be7c7d3e778897cf78d8bb59f76d3fa5d718d974
maxine-janka <maxinejanka@gmail.com>
2024-12-11 03:39:29 +0000

    Super simple shell exercise - runs a command with full path

---
+++ shell_concept_exercises/simple.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <string.h>
+
+int main(void)
+{
+	char *buff = NULL;
+	size_t buff_size;
+	char *token;
+	int i = 0, status;
+	char **str;
+	pid_t child;
+	
+	while (1) /*while true*/
+	{
+		write(1, "$ ", 2); /*print prompt*/
+		getline(&buff, &buff_size, stdin);
+		str = malloc(sizeof(char *) * 256);
+
+		token = strtok(buff," \t\n\r"); /*collect first token*/
+		
+		while (token != NULL) /*array gets rest of tokens*/
+		{
+			str[i] = token;
+			token = strtok(NULL, " \t\n\r"); 
+			i++;
+		}
+		str[i] = '\0'; /*end with null term*/
+		child = fork();
+		if (child == -1)
+		{
+			perror("error");
+			return (1);
+		}
+
+		if (child == 0)
+		{
+			if (execve(str[0], str, NULL) == -1)
+				perror("error");
+				exit (1);
+		}	
+		else
+		{
+			wait(&status);
+		}
+		i = 0;
+		free(str);
+	}
+	return (0);
+}
