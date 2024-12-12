0130d753119ae07e3af69a2c7412ec8ffe2078dd
maxine-janka <maxinejanka@gmail.com>
2024-12-12 11:03:21 +0000

    test shell 0.1

--- shell_concept_exercises/test_shell.c
+++ shell_concept_exercises/test_shell.c
@@ -24,7 +24,7 @@ char *read_line(void)
         return NULL;
     }
 
-    if (buffer[bytes_read - 1] == '\n')
+	if (buffer[bytes_read - 1] == '\n')
         buffer[bytes_read - 1] = '\0';
 
     return buffer;
@@ -36,25 +36,41 @@ char *read_line(void)
  *
  * Return: None.
  */
-void execute_command(char *command)
+void execute_command(char *line)
 {
-    pid_t child_pid;
-    int status;
-    char *argv[20];
+	char *token;
+	int i = 0, status;
+	pid_t child;
+	char **str;
 
-    argv[0] = command;
-    argv[1] = NULL;
+	str = malloc(sizeof(char *) * 256);
+	if (str == NULL)
+	{
+		perror("malloc fail");
+		exit(EXIT_FAILURE);
+	}
 
-    child_pid = fork();
-    if (child_pid == -1)
+	token = strtok(line, " \n\t");
+	while (token != NULL)
+	{
+		str[i] = token;
+		token = strtok(NULL, " \n\t");
+		i++;
+	}
+	str[i] = NULL;
+
+
+    child = fork();
+    if (child == -1)
     {
         perror("Error forking");
+	free(str);
         return;
     }
 
-    if (child_pid == 0)
+    if (child == 0)
     {
-        if (execve(command, argv, NULL) == -1)
+        if (execve(str[0], str, NULL) == -1)
         {
             perror("Error");
             exit(EXIT_FAILURE);
@@ -63,6 +79,7 @@ void execute_command(char *command)
     else
     {
         wait(&status);
+	free(str);
     }
 }
 
